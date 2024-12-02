f16256e286e5859b0047b2b0fa91c75ab0809c21
wsp8521 <wsp8521@hotmail.com>
2024-12-02 16:03:11 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 from interface.interface import ChatbootInterface
 
-
 interface = ChatbootInterface()
-
 
 interface.config_page(page_title='Chat Porto do Itaqui',page_icon='📃')
 interface.chat_window()
