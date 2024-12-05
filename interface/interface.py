fbf1f4bb6acb17a2ea1fc1679875ea0c44ecaa80
wsp8521 <wsp8521@hotmail.com>
2024-12-04 21:46:31 -0300

    Add files via upload

--- interface/interface.py
+++ interface/interface.py
@@ -1,33 +1,59 @@
+import os
 import streamlit as st
 from chat.chatboot import ChatBoot
-from langchain.memory import ConversationBufferMemory
-
+from langchain_openai import  ChatOpenAI
+from langchain_groq import ChatGroq
 
 
 class ChatbootInterface:
-    def __init__(self):
+    def __init__(self, model_apenai:bool=True, model_groq:bool=True):
+        self.openai_activate = model_apenai
+        self.groq_activate = model_groq
         self.chatboot = ChatBoot(model="llama-3.2-90b-vision-preview")
-        self.models =  {'Groq':{'modelos':['llama-3.2-90b-vision-preview','llama-3.1-70b-versatile','gemma2-9b-it'] },
-                        'Openai':{'modelos':['gpt-4o-mini','gpt-3.5-turbo-0125']}}
-
-    
-        
-    #config da página
-    def config_page(self, page_title, page_icon=None ):
-        st.set_page_config(page_title=page_title, page_icon=page_icon)
-        
-
+        self.models =  {
+                        'Groq':{'modelos':['llama-3.2-90b-vision-preview','llama-3.1-70b-versatile','gemma2-9b-it'],
+                                'chat':'ChatGroq', 'api':os.getenv('TOKEN_GROQ')},
+                        'Openai':{'modelos':['gpt-4o-mini','gpt-3.5-turbo-0125', 'gpt-4o'],
+                                  'chat': 'ChatOpenAI', 'api':os.getenv('TOKEN_OPENAI')}
+                        }
+        self.__select_model()
+ 
      #conteúdos da sidebar
     def sidebar(self):
         with st.sidebar:
            get_file = st.file_uploader("upload arquivo pdf", accept_multiple_files=True, type=['.pdf'])
-           
+
         with st.spinner("processando documento"):
            self.chatboot.process_documents(get_file)
+           
+                 
+    #seleção de modelo       
+    def __select_model(self):
+        st.sidebar.header('🤖 Modelo de IA')
+        model_apenai = self.openai_activate
+        model_groq = self.groq_activate
+        
+         #identificação dos rovedores
+        if model_apenai and model_groq==False:
+           provedor =  list(self.models.keys())[1] #exibe apenas os modelos da openais
+        elif model_apenai==False and model_groq:
+            provedor =  list(self.models.keys())[0] #exibe apenas os modelos da Groq
+        else:
+            provedor = self.models.keys() #exibe os dois modelos
+            
+            
+        descLabel_provedor = 'selecione o provedor do modelo' if model_apenai==False and model_groq==False else ""
+        provedor_model = st.sidebar.selectbox(descLabel_provedor, provedor)
+        models = st.sidebar.selectbox('selecione o modelo', self.models[provedor_model]['modelos']),
+        api = self.models[provedor_model]['api']
+        
+        chat = self.models[provedor]['chat'](model = models, api_key = api)
+        #return chat
+        
 
     #área do chat    
     def chat_window(self):
-        st.header('🤖 Chat Port', divider=True)
+        st.header('🚢 Chat Porto Itaqui', divider=True)
         vector_store = self.chatboot.load_vector_store()
                 
         if vector_store is None: #verifica se ha banco de dados
